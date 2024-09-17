d9d97d80cdef2051a4a9acdc5e5f2581c13821a6
Thorsten Roeske <thorsten@wearonize.com>
2024-09-17 15:36:01 +0000

    add: register home admin sites

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Consultant, Skill, PastEmployment, PressLink
 
 # Register your models here.
+admin.site.register(Consultant)
+admin.site.register(Skill)
+admin.site.register(PastEmployment)
+admin.site.register(PressLink)
