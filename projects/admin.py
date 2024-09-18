688230b93b309b018e5b271c8afbfaceef84f4aa
Thorsten Roeske <thorsten@wearonize.com>
2024-09-18 11:23:56 +0000

    add: admin views

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project, Category, Learning, Section
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Category)
+admin.site.register(Learning)
+admin.site.register(Section)
