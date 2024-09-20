2201639bb33a82c4eccc08e065465e8df388ebf4
Thorsten Roeske <thorsten@wearonize.com>
2024-09-20 06:54:37 +0000

    add: email confirmation on collaboration request

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import CollaborationRequest
 from .forms import CollaborationForm
 from django.core.mail import send_mail
+from home.models import Consultant, Config
+
+def get_consultant_name():
+    """
+    Get the consultant id from the Config table 
+    and first and last name from consultant table
+    """
+    queryset = Config.objects.all()
+    current_consultant = get_object_or_404(queryset, key="CURRENT_CONSULTANT")
+    consultant = Consultant.objects.filter(consultant_id=current_consultant.value).first()
+
+    return consultant.first_name, consultant.last_name
 
 # Create your views here.
 def collaboration_request_list(request):
@@ -29,11 +41,24 @@ def contact(request):
         collaboration_form = CollaborationForm(data=request.POST)
         if collaboration_form.is_valid():
             collaboration_form.save()
+            
+            current_cr = CollaborationRequest.objects.filter(open=True).order_by("-request_date").first()
+            consultant_first_name, consultant_last_name = get_consultant_name()
+
+            message = (
+                f"Hi {current_cr.first_name},\n\n"
+                "Thanks for your message!\n\n"
+                "We have received your collaboration request for "
+                f"{consultant_first_name} {consultant_last_name}" 
+                " and will respond at the earliest convenience.\n"
+                "\nKind regards"
+            )
+            
             send_mail(
-                "Collaboration Request received",
-                "thank you for reaching out",
+                "[NO-REPLY] Collaboration Request received",
+                message,
                 "tsroeske@gmail.com",
-                ["tsroeske@gmail.com"],
+                [current_cr.email],
                 fail_silently=False,
             )
 
