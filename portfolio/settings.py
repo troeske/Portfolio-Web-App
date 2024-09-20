62207400c691ed125d4ba29ab267ef397ef41437
Thorsten Roeske <thorsten@wearonize.com>
2024-09-20 18:29:05 +0000

    fix: debug true for testing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
