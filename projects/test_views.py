189fe3815ab527cdac70a0aae6b6b860b1e86e45
Thorsten Roeske <thorsten@wearonize.com>
2024-09-24 04:51:16 +0000

    add: unittests for projects/views/approve_client and projects/views/delete_client

--- projects/test_views.py
+++ projects/test_views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.test import TestCase
-from unittest.mock import patch, MagicMock
+from django.test import TestCase, Client as TestClient
+from unittest.mock import patch
 from .models import Project, Client
 from home.models import Consultant
 
 
 class TestProjectsListView(TestCase):
-
+    """Testing thje projects list view
+        
+    """
+    
     def setUp(self):
 
         self.user = User.objects.create_user(
@@ -62,4 +65,76 @@ class TestProjectsListView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertNotIn(b"Project title", response.content)
 
-       
+
+class TestClientDeleteView(TestCase):
+    """Testing the client delete view
+        
+    """
+    
+    def setUp(self):
+        self.test_client = TestClient()
+        self.superuser = User.objects.create_superuser(
+            username='superuser', 
+            password='password123', 
+            email='superuser@test.com'
+        )
+        self.normal_user = User.objects.create_user(
+            username='normaluser', 
+            password='password123', 
+            email='normaluser@test.com'
+        )
+    
+    def test_superuser_can_delete_client(self):
+        self.test_client.login(username='superuser', password='password123')
+        response = self.test_client.post(reverse('client_delete', args=['normaluser']))
+        self.assertFalse(User.objects.filter(username='normaluser').exists())
+
+    def test_non_superuser_cannot_delete_client(self):
+        self.test_client.login(username='normaluser', password='password123')
+        response = self.test_client.post(reverse('client_delete', args=['superuser']))
+        self.assertTrue(User.objects.filter(username='superuser').exists())
+
+
+class TestApproveClientView(TestCase):
+    """Testing the approve client view
+        
+    """
+    
+    def setUp(self):
+        self.test_client = TestClient()
+        self.superuser = User.objects.create_superuser(
+            username='superuser', 
+            password='password123',
+            email='superuser@test.com'
+        )
+
+        self.normal_user = User.objects.create_user(
+            username='normaluser', 
+            password='password123',
+            email='normaluser@test.com'
+        )
+
+        self.consultant = Consultant.objects.create(
+            consultant_id=1,
+            first_name="Test",
+            last_name="Consultant",
+            email="test@test.com"
+        )
+
+        self.client_instance = Client.objects.create(
+            client = self.normal_user,
+            approved=False,
+            allow_delete=True,
+            consultant = self.consultant,
+            email="test@test.com"
+        )
+    
+    def test_superuser_can_approve_client(self):
+        self.test_client.login(username='superuser', password='password123')
+        response = self.test_client.post(reverse('approve_client', args=[self.client_instance.id]))
+        self.assertTrue(Client.objects.get(client=self.normal_user).approved)
+    
+    def test_normaluser_cannot_approve_client(self):
+       self.test_client.login(username='normaluser', password='password123')
+       response = self.test_client.post(reverse('approve_client', args=[self.client_instance.id]))
+       self.assertFalse(Client.objects.get(client=self.normal_user).approved)
